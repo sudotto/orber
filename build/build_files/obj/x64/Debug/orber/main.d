727480647f7a18e23545f2b53756b85a1d038983
sudotto <codekiwi132@gmail.com>
2026-01-12 23:36:33 -0800

    here you go pookie <3

---
+++ build/build_files/obj/x64/Debug/orber/main.d
@@ -0,0 +1,11 @@
+obj/x64/Debug/orber/main.o: ../../src/main.c /usr/include/stdc-predef.h \
+ ../external/raylib-master/src/raylib.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdarg.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdbool.h \
+ ../../include/resource_dir.h ../../src/player.h
+/usr/include/stdc-predef.h:
+../external/raylib-master/src/raylib.h:
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdarg.h:
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdbool.h:
+../../include/resource_dir.h:
+../../src/player.h:
---
+++ build/build_files/obj/x64/Debug/orber/player.d
@@ -0,0 +1,10 @@
+obj/x64/Debug/orber/player.o: ../../src/player.c \
+ /usr/include/stdc-predef.h ../../src/player.h \
+ ../external/raylib-master/src/raylib.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdarg.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdbool.h
+/usr/include/stdc-predef.h:
+../../src/player.h:
+../external/raylib-master/src/raylib.h:
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdarg.h:
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdbool.h:
